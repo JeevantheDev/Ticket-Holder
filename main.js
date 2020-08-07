b257e17278d37a6b61d108dce534a333bd9dde0e
Jeevan Jyoti Dash <dashjeevanjyoti04@gmail.com>
2020-08-07 19:27:38 +0530

    new update

--- main.js
+++ main.js
@@ -96,15 +96,14 @@ class Store {
 
       // Add plan
       ui.addPlanToList(plan);
-
-      //   Select option
-      var elems = document.querySelectorAll("select");
-      var instances = M.FormSelect.init(elems);
-
-      //   Date picker
-      var elems = document.querySelectorAll(".datepicker");
-      var instances = M.Datepicker.init(elems);
     });
+    //   Select option
+    var elems = document.querySelectorAll("select");
+    var instances = M.FormSelect.init(elems);
+
+    //   Date picker
+    var elems = document.querySelectorAll(".datepicker");
+    var instances = M.Datepicker.init(elems);
   }
 
   static addPlan(plan) {
